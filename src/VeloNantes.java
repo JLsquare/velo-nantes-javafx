0b6feed44d21f076b59bbe42cdef64e26463f1b5
JL² <jeanloup.mellion@gmail.com>
2023-05-25 09:26:22 +0200

    Import fxml + refactor

---
+++ src/VeloNantes.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class VeloNantes extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/VeloNantes.fxml"));
+            Scene scene = new Scene(root, 1080, 720);
+            primaryStage.setTitle("Velo de Nantes");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
