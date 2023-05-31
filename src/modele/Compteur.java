251bdd727734ffcf58aa9065e8e27f697f796ba7
JL² <jeanloup.mellion@gmail.com>
2023-05-31 12:04:26 +0200

    database + modele

---
+++ src/modele/Compteur.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Compteur{
+    private int idCompteur;
+    private String nomCompteur;
+    private float coordX;
+    private float coordY;
+    private Quartier leQuartier;
+
+    public Compteur(int idCompteur, String nomCompteur, float coordX, float coordY, Quartier leQuartier) {
+        this.idCompteur = idCompteur;
+        this.nomCompteur = nomCompteur;
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.leQuartier = leQuartier;
+    }
+
+    public int getIdCompteur() {
+        return idCompteur;
+    }
+
+    public void setIdCompteur(int idCompteur) {
+        this.idCompteur = idCompteur;
+    }
+
+    public String getNomCompteur() {
+        return nomCompteur;
+    }
+
+    public void setNomCompteur(String nomCompteur) {
+        this.nomCompteur = nomCompteur;
+    }
+
+    public float getCoordX() {
+        return coordX;
+    }
+
+    public void setCoordX(float coordX) {
+        this.coordX = coordX;
+    }
+
+    public float getCoordY() {
+        return coordY;
+    }
+
+    public void setCoordY(float coordY) {
+        this.coordY = coordY;
+    }
+
+    public Quartier getLeQuartier() {
+        return leQuartier;
+    }
+
+    public void setLeQuartier(Quartier leQuartier) {
+        this.leQuartier = leQuartier;
+    }
+}
---
+++ src/modele/Quartier.java
@@ -0,0 +1,184 @@
+package modele;
+
+import java.util.ArrayList;
+import java.sql.*;
+
+
+/**
+ * Class Quartier who represent a MySQL table
+ * It's a model who can contain all attributes and methods to access at data
+ * It also use JDBC to interact with the database
+ * @author Groupe 4B2
+ */
+public class Quartier {
+
+    // ---------------- Attributes ---------------- //
+
+    private int idQuartier;
+    private String nomQuartier;
+    private float longueurPisteVelo;
+    private ArrayList<Compteur> lesCompteurs;
+
+    // ---------------- Constructors ---------------- //
+
+    /**
+     * Default constructor
+     * @param idQuartier the id of the Quartier
+     * @param nomQuartier the name of the Quartier
+     * @param longueurPisteVelo the length of the bike path
+     */
+    public Quartier(int idQuartier, String nomQuartier, float longueurPisteVelo) {
+        this.idQuartier = idQuartier;
+        this.nomQuartier = nomQuartier;
+        this.longueurPisteVelo = longueurPisteVelo;
+        this.lesCompteurs = new ArrayList<Compteur>();
+    }
+
+    /**
+     * Constructor with ArrayList of Compteur
+     * @param idQuartier the id of the Quartier
+     * @param nomQuartier the name of the Quartier
+     * @param longueurPisteVelo the length of the bike path
+     * @param lesCompteurs the ArrayList of Compteur
+     * @throws NullPointerException if lesCompteurs is null
+     */
+    public Quartier(int idQuartier, String nomQuartier, float longueurPisteVelo, ArrayList<Compteur> lesCompteurs) throws NullPointerException{
+        if(lesCompteurs == null){
+            throw new NullPointerException("lesCompteurs ne peut pas être null");
+        }
+        this.idQuartier = idQuartier;
+        this.nomQuartier = nomQuartier;
+        this.longueurPisteVelo = longueurPisteVelo;
+        this.lesCompteurs = lesCompteurs;
+    }
+
+    /**
+     * Constructor with ResultSet
+     * @param rs the ResultSet
+     * @throws SQLException if there is an error with the ResultSet
+     */
+    public Quartier(ResultSet rs) throws SQLException{
+        this.idQuartier = rs.getInt("idQuartier");
+        this.nomQuartier = rs.getString("nomQuartier");
+        this.longueurPisteVelo = rs.getFloat("longueurPisteVelo");
+        this.lesCompteurs = new ArrayList<Compteur>();
+    }
+
+    // ---------------- Getters & Setters ---------------- //
+
+    /**
+     * Get the id of the Quartier
+     * @return the id of the Quartier
+     */
+    public int getIdQuartier() {
+        return idQuartier;
+    }
+
+    /**
+     * Set the id of the Quartier
+     * @param idQuartier the id of the Quartier
+     */
+    public void setIdQuartier(int idQuartier) {
+        this.idQuartier = idQuartier;
+    }
+
+    /**
+     * Get the name of the Quartier
+     * @return the name of the Quartier
+     */
+    public String getNomQuartier() {
+        return nomQuartier;
+    }
+
+    /**
+     * Set the name of the Quartier
+     * @param nomQuartier the name of the Quartier
+     * @throws NullPointerException if nomQuartier is null
+     */
+    public void setNomQuartier(String nomQuartier) throws NullPointerException{
+        if(nomQuartier == null){
+            throw new NullPointerException("nomQuartier ne peut pas être null");
+        }
+        this.nomQuartier = nomQuartier;
+    }
+
+    /**
+     * Get the length of the bike path
+     * @return the length of the bike path
+     */
+    public float getLongueurPisteVelo() {
+        return longueurPisteVelo;
+    }
+
+    /**
+     * Set the length of the bike path
+     * @param longueurPisteVelo the length of the bike path
+     */
+    public void setLongueurPisteVelo(float longueurPisteVelo) {
+        this.longueurPisteVelo = longueurPisteVelo;
+    }
+
+    // ---------------- Compteur ---------------- //
+
+    /**
+     * Add a Compteur to the ArrayList of Compteur
+     * @param unCompteur the Compteur to add
+     * @throws NullPointerException if unCompteur is null
+     */
+    public void addCompteur(Compteur unCompteur) throws NullPointerException{
+        if(unCompteur == null){
+            throw new NullPointerException("unCompteur ne peut pas être null");
+        }
+        this.lesCompteurs.add(unCompteur);
+    }
+
+    /**
+     * Get a Compteur from the ArrayList of Compteur
+     * @param index the index of the Compteur
+     * @return the Compteur
+     * @throws IndexOutOfBoundsException if index is out of bounds
+     */
+    public Compteur getCompteur(int index) throws IndexOutOfBoundsException{
+        if(index < 0 || index >= this.lesCompteurs.size()){
+            throw new IndexOutOfBoundsException("index doit être compris entre 0 et " + (this.lesCompteurs.size() - 1));
+        }
+        return this.lesCompteurs.get(index);
+    }
+
+    /**
+     * Remove a Compteur from the ArrayList of Compteur
+     * @param index the index of the Compteur
+     * @throws IndexOutOfBoundsException if index is out of bounds
+     */
+    public void removeCompteur(int index) throws IndexOutOfBoundsException{
+        if(index < 0 || index >= this.lesCompteurs.size()){
+            throw new IndexOutOfBoundsException("index doit être compris entre 0 et " + (this.lesCompteurs.size() - 1));
+        }
+        this.lesCompteurs.remove(index);
+    }
+
+    // ---------------- Other ---------------- //
+
+    /**
+     * To String method
+     * @return the String of the Quartier
+     */
+    @Override
+    public String toString() {
+        String string = "Quartier{" + "idQuartier : " + this.idQuartier + ", nomQuartier : " + this.nomQuartier + ", longueurPisteVelo : " + this.longueurPisteVelo + '}';
+        return string;
+    }
+
+    /**
+     * Convert the Quartier to a Insert SQL Query
+     * @return the Insert SQL Query
+     */
+    public PreparedStatement toInsertQuery() throws SQLException, NoDatabaseException{
+        Connection connection = Database.getWriteConnection();
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO Quartier (idQuartier, nomQuartier, longueurPisteVelo) VALUES (?, ?, ?)");
+        statement.setInt(1, this.idQuartier);
+        statement.setString(2, this.nomQuartier);
+        statement.setFloat(3, this.longueurPisteVelo);
+        return statement;
+    }
+}
